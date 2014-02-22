1e62352a1a0b9e67b488cf4e2738481a998ea74b
Sylae Corell <sylae@calref.net>
2014-02-22 11:22:41 -0700

    A more-random function for dice rolls
    Definitely overkill

--- ligparse.php
+++ ligparse.php
@@ -10,8 +10,22 @@ function send($room, $text) {
 }
 
 function rollDice($sides) {
-  mt_srand((double)microtime()*1000000);
-  return mt_rand(1, $sides);
+  return crypto_rand_secure(1, $sides);
+}
+
+// http://us3.php.net/manual/en/function.openssl-random-pseudo-bytes.php#104322
+function crypto_rand_secure($min, $max) {
+  $range = $max - $min;
+  if ($range == 0) return $min; // not so random...
+  $log = log($range, 2);
+  $bytes = (int) ($log / 8) + 1; // length in bytes
+  $bits = (int) $log + 1; // length in bits
+  $filter = (int) (1 << $bits) - 1; // set all lower bits to 1
+  do {
+    $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes, $s)));
+    $rnd = $rnd & $filter; // discard irrelevant bits
+  } while ($rnd >= $range);
+  return $min + $rnd;
 }
 
 function parseCustomCommands($text, $textParts, $room, $res) { 
