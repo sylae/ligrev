63ebae0aa56f805bebea4ae2696de8e9fb524fc6
Sylae Corell <sylae@calref.net>
2015-03-29 17:44:43 -0600

    formatting

--- ligrev.php
+++ ligrev.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Ligrev;
+
 define("L_DEBUG", 0);
 define("L_INFO", 1);
 define("L_CAUT", 2);
@@ -9,33 +11,31 @@ define("L_AAAA", 4);
 // Default error reporting level
 define("L_REPORT", L_DEBUG);
 
-
 // Take over PHP's error handling, since it's a picky whore sometimes.
 function php_error_handler($no, $str, $file, $line) {
   switch ($no) {
     case E_ERROR:
     case E_RECOVERABLE_ERROR:
-      l("[PHP] ".$str. " at ".$file.":".$line, L_AAAA);
+      l("[PHP] " . $str . " at " . $file . ":" . $line, L_AAAA);
       die();
       break;
     case E_WARNING:
     case E_PARSE:
-      l("[PHP] ".$str. " at ".$file.":".$line, L_WARN);
+      l("[PHP] " . $str . " at " . $file . ":" . $line, L_WARN);
       break;
     case E_NOTICE:
-      l("[PHP] ".$str. " at ".$file.":".$line, L_CAUT);
+      l("[PHP] " . $str . " at " . $file . ":" . $line, L_CAUT);
       break;
     case E_DEPRECATED:
     case E_STRICT:
-      l("[PHP] ".$str. " at ".$file.":".$line, L_DEBUG);
+      l("[PHP] " . $str . " at " . $file . ":" . $line, L_DEBUG);
       break;
     default:
-      l("[PHP] ".$str. " at ".$file.":".$line, L_INFO);
+      l("[PHP] " . $str . " at " . $file . ":" . $line, L_INFO);
       break;
   }
   return true;
 }
-
 
 // Function to log/echo to the console. Includes timestamp and what-not
 function l($text, $level = L_INFO) {
@@ -66,7 +66,7 @@ function l($text, $level = L_INFO) {
 
 set_error_handler("Ligrev\\php_error_handler");
 
-set_include_path(get_include_path().PATH_SEPARATOR.__DIR__.DIRECTORY_SEPARATOR.'lib');
+set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ . DIRECTORY_SEPARATOR . 'lib');
 
 // Hey, let's load some things
 l("Reading config.php...");
@@ -89,37 +89,41 @@ l("[JAXL] Loading JAXL and connecting...");
 $client = new \JAXL($config['jaxl']);
 
 $client->require_xep(array(
-	'0045',	// MUC
-	'0203',	// Delayed Delivery
-	'0199'  // XMPP Ping
+  '0045', // MUC
+  '0203', // Delayed Delivery
+  '0199'  // XMPP Ping
 ));
 
 $rooms = array();
 
 $client->add_cb('on_auth_success', function() {
-	global $client, $config, $rooms;
-	l("[JAXL] Connected with jid ".$client->full_jid->to_string());
+  global $client, $config, $rooms;
+  l("[JAXL] Connected with jid " . $client->full_jid->to_string());
   $client->get_vcard();
   $client->get_roster();
   $client->set_status("", "chat", 10);
-  
+
   foreach ($config['rooms'] as $id => $jid) {
-    $rooms[$id] = new \XMPPJid($jid.'/'.$config['botname']);
-    l("[JAXL] Joining room ".$rooms[$id]->to_string());
+    $rooms[$id] = new \XMPPJid($jid . '/' . $config['botname']);
+    l("[JAXL] Joining room " . $rooms[$id]->to_string());
     $client->xeps['0045']->join_room($rooms[$id]);
-    l("[JAXL] Joined room ".$rooms[$id]->to_string());
+    l("[JAXL] Joined room " . $rooms[$id]->to_string());
   }
 });
 
 $client->add_cb('on_auth_failure', function($reason) {
-	global $client;
-	$client->send_end_stream();
-	l("[JAXL] Auth failure: ".$reason, L_WARN);
+  global $client;
+  $client->send_end_stream();
+  l("[JAXL] Auth failure: " . $reason, L_WARN);
 });
 
 // Where the magic happens. "Magic" "Happens". I dunno why I type this either.
-$client->add_cb('on_groupchat_message', function($stanza){new ligrevCommand($stanza, "groupchat");});
-$client->add_cb('on_chat_message', function($stanza){new ligrevCommand($stanza, "chat");});
+$client->add_cb('on_groupchat_message', function($stanza) {
+  new ligrevCommand($stanza, "groupchat");
+});
+$client->add_cb('on_chat_message', function($stanza) {
+  new ligrevCommand($stanza, "chat");
+});
 
 $message_type = null;
 $client->start();
