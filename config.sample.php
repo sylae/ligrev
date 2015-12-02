9a5d3fcebdfec5d312c0ead070cf4cf6f9231e43
Sylae Corell <sylae@calref.net>
2015-12-02 01:25:13 -0700

    Clean up sample config

--- config.sample.php
+++ config.sample.php
@@ -4,20 +4,26 @@ $config = array();
 // DSN sent to DBAL
 $config['db'] = 'mysqli://user:password@localhost/database';
 
+// Bot credentials
 $config['jaxl'] = array(
-	'jid' => 'ligrev@example.net',
-	'pass' => 'rainbowdashisactuallybestpony',
-	'host' => 'example.net',
+  'jid' => 'ligrev@example.net',
+  'pass' => 'rainbowdashisactuallybestpony',
+  'host' => 'example.net',
 );
+$config['botname'] = "Ligrev";
 
+// List of rooms to join
 $config['rooms'] = array(
   'lounge' => 'lounge@conference.example.net',
   'test'   => 'test@conference.example.org',
 );
 
+// :tell settings
+// defaultTellDomain automatically provides a domain if not provided in the :tell command
+// (ie ':tell sylae message' would become ':tell sylae@example.net message')
 $config['defaultTellDomain'] = 'example.net';
 $config['tellCaseSensitive'] = true;
 
-$config['botname'] = "Ligrev";
+// Editing below this point is not needed in most cases.
 
 $config['bclibs'] = 'lib/phodd/array.bc lib/phodd/digits.bc lib/phodd/digits_describe.bc lib/phodd/digits_happy.bc lib/phodd/digits_misc.bc lib/phodd/factorial.bc lib/phodd/factorial_gamma.bc lib/phodd/fibonacci.bc lib/phodd/funcs.bc lib/phodd/intdiff.bc lib/phodd/interest.bc lib/phodd/logic.bc lib/phodd/logic_andm.bc lib/phodd/logic_inverse.bc lib/phodd/logic_otherbase.bc lib/phodd/logic_striping.bc lib/phodd/logic_striping_meta.bc lib/phodd/melancholy.bc lib/phodd/melancholy_b.bc lib/phodd/misc_235.bc lib/phodd/misc_ack.bc lib/phodd/misc_anglepow.bc lib/phodd/misc_perfectpow.bc lib/phodd/misc_srr.bc lib/phodd/orialc.bc lib/phodd/output_formatting.bc lib/phodd/output_roman.bc lib/phodd/primes.bc lib/phodd/primes_db_code.bc lib/phodd/primes_db_minipack.bc lib/phodd/primes_other.bc lib/phodd/primes_twin.bc lib/phodd/rand/rand.bc lib/phodd/thermometer.bc lib/bcmisc/minmax.bc';
