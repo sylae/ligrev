571daf1147e8a64bdad8d3043ae132c114294693
Sylae Corell <sylae@calref.net>
2015-12-21 09:38:52 -0700

    Support responding to XEP-0202 when asked for the time

---
+++ iq/xep-0202.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Support for XEP-0202, Entity Time
+ *
+ * @link http://xmpp.org/extensions/xep-0202.html
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU General Public License 3
+ * @author Sylae Jiendra Corell <sylae@calref.net>
+ */
+
+namespace Ligrev\IQ;
+
+class xep_0202 extends \Ligrev\iq {
+
+  static function canUse(\XMPPStanza $stanza, \QueryPath\DOMQuery $qp) {
+    return (\qp($qp, 'time')->attr('xmlns') == NS_TIME && $stanza->type == "get");
+  }
+
+  function process(\XMPPStanza $stanza) {
+
+    $resp = new \XMPPIq(array('from' => $this->client->full_jid->to_string(), 'to' => $stanza->from, 'type' => 'result', 'id' => $stanza->id));
+    $resp->c('time', NS_TIME);
+    $resp->c('utc', null, array(), date(DATE_ATOM))->up();
+    $resp->c('tzo', null, array(), date("P"));
+
+    $this->client->send($resp);
+  }
+
+}
