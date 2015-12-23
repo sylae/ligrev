600e176b41397b1521cc0988649d68428238e0a2
Sylae Corell <sylae@calref.net>
2015-12-22 19:23:28 -0700

    formatting

--- config.sample.php
+++ config.sample.php
@@ -1,16 +1,16 @@
 <?php
+
 /**
  * Sample configuration for ligrev. To make changes, copy to config.php
  * and make your edits there.
- * 
+ *
  * This file documents the bare-bones minimum to get Ligrev started and running.
  * For more details, see the wiki's documentation.
- * 
+ *
  * @link https://github.com/sylae/ligrev/wiki
  * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU General Public License 3
  * @author Sylae Jiendra Corell <sylae@calref.net>
  */
-
 $config['db'] = 'mysqli://user:password@localhost/database';
 
 $config['jaxl'] = array(
